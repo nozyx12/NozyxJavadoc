0ca78079284b2bad0a2dab3a4508716cd9c7d83c
Ezio Nozyx <nozyxoff@proton.me>
2024-10-26 22:02:41 +0200

    Added OpenToast v1.0

---
+++ opentoast/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.nozyx12.opentoast"},{"l":"io.github.nozyx12.opentoast.modifier"},{"l":"io.github.nozyx12.opentoast.style"}];updateSearchResults();
